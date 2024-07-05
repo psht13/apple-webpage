9489a16438e166be4dcdf0e16859f58072d7e928
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-07-05 09:09:18 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "pavlo-6l",
-    project: "javascript-react"
-  })],
+  plugins: [react()],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
